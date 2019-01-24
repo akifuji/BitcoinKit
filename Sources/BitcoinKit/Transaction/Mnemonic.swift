c883f3fd3f6f27f30d0675f216696a3f0de7b883
藤田哲史 <akifuji@protonmail.com>
2019-01-24 14:37:03 +0900

    update Mnemonic

--- Sources/BitcoinKit/Transaction/Mnemonic.swift
+++ Sources/BitcoinKit/Transaction/Mnemonic.swift
@@ -1,9 +1,26 @@
 //
 //  Mnemonic.swift
-//  BitcoinKit
 //
-//  Created by Akifumi Fujita on 2019/01/23.
-//  Copyright © 2019 BitcoinKit developers. All rights reserved.
+//  Copyright © 2018 Kishikawa Katsumi
+//  Copyright © 2018 BitcoinKit developers
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
@@ -22,7 +39,7 @@ public struct Mnemonic {
         case veryHigh = 256
     }
 
-    public enum  Language {
+    public enum Language {
         case english
         case japanese
         case korean
@@ -33,22 +50,22 @@ public struct Mnemonic {
         case italian
     }
 
-    public static func generate(strength: Strength = .default, langeage: Language) throws -> [String] {
+    public static func generate(strength: Strength = .default, language: Language = .english) throws -> [String] {
         let byteCount = strength.rawValue / 8
         var bytes = Data(count: byteCount)
         let status = bytes.withUnsafeMutableBytes { SecRandomCopyBytes(kSecRandomDefault, byteCount, $0) }
-        guard status == errSecSuccess else {
-            throw MnemonicError.randomBytesError
-        }
-        return generate(entropy: bytes, language: langeage)
+        guard status == errSecSuccess else { throw MnemonicError.randomBytesError }
+        return generate(entropy: bytes, language: language)
     }
 
-    private static func generate(entropy: Data, language: Language) -> [String] {
+    internal static func generate(entropy: Data, language: Language = .english) -> [String] {
         let list = wordList(for: language)
         var bin = String(entropy.flatMap { ("00000000" + String($0, radix: 2)).suffix(8) })
+
         let hash = Crypto.sha256(entropy)
         let bits = entropy.count * 8
         let cs = bits / 32
+
         let hashbits = String(hash.flatMap { ("00000000" + String($0, radix: 2)).suffix(8) })
         let checksum = String(hashbits.prefix(cs))
         bin += checksum
@@ -62,7 +79,7 @@ public struct Mnemonic {
     }
 
     public static func seed(mnemonic m: [String], passphrase: String = "") -> Data {
-        let mnemonic = m.joined(separator: "").decomposedStringWithCompatibilityMapping.data(using: .utf8)!
+        let mnemonic = m.joined(separator: " ").decomposedStringWithCompatibilityMapping.data(using: .utf8)!
         let salt = ("mnemonic" + passphrase).decomposedStringWithCompatibilityMapping.data(using: .utf8)!
         let seed = _Key.deriveKey(mnemonic, salt: salt, iterations: 2048, keyLength: 64)
         return seed
